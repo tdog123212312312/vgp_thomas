25212d00503b13e1c940cebba18b25fce2c37214
tdog123212312312 <113127068+tdog123212312312@users.noreply.github.com>
2022-10-12 09:22:46 -0700

    added the project

---
+++ My project (6)/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
