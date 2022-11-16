c5e5a9682e8dc822170f57d280fc4fb741b1348e
tdog123212312312 <113127068+tdog123212312312@users.noreply.github.com>
2022-11-16 09:15:02 -0800

    wderftgyujik

---
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = get
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
