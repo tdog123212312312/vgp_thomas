9f310b8181ccb0a6c2c6028a5b9c07c0a03b42e2
tdog123212312312 <113127068+tdog123212312312@users.noreply.github.com>
2022-11-17 09:13:51 -0800

    werftgyhujikol

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,31 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = get
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        { 
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        } 
+
+        private void OnCollisionEnter(Collision collision);  
+        {
+            isOnGround = true;
+        }  
     }
 }
