5378db4a9bd15fa12c3e061ec03b15dbb1d9aaf7
tdog123212312312 <113127068+tdog123212312312@users.noreply.github.com>
2022-12-13 15:05:25 -0800

    aa
    aa

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,10 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,28 +28,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         { 
+            dirtParticle.Stop();
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig"); 
+
         } 
-
-         
+        
     }
-     private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            
-
-            if(collision.gameObject.CompareTag("Ground"))
-
-        {
+            dirtParticle.Play();
             isOnGround = true;
-
-        } else if(collision.gameObject.CompareTag("Obstacle"))
+            Debug.Log("Still Playing");
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
         {
-            Debug.Log("Game Over You Suck Take The L Loser");
+            dirtParticle.Stop();
+            Debug.Log("Game Over");
             gameOver = true;
-            playerAnim.SetBool("Death_")
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
         }
-        }
+    }
 }
+
